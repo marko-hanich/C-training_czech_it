a26486cb1dd2683066672ed1e2eaf04228cc5045
marko-hanich <ganichmark2017@gmail.com>
2022-10-19 19:52:43 +0200

    C-training

---
+++ training.c
@@ -0,0 +1,47 @@
+// #include <stdio.h>
+// int main () {
+//     int num = 12, res;
+//     float x;
+//     x = 23.3;
+//     res = x - num;
+//     printf("Result is %d, x is %.1f\n", res, x);
+//     return 0;
+
+// }
+
+
+
+// #include <stdio.h>
+// int main () {
+//     int age;
+
+//     printf("Enter your age: ");
+//     scanf("%d", &age);
+
+//     if (age > 18) {
+//     printf("Congratulation, you can visit our site! Your age is: %d\n", age);
+//     }
+//     else if (age < 18)
+//     {
+//         printf("You are too small. Your age is: %d\n", age);
+//     }
+//     else {
+//         printf("Happy birthday! Your age is: %d\n", age);
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+int main() {
+    int x = 2, y = 4;
+    int c;
+    c = x > y ? 23 : 45;
+    if (x>y) {
+        c = 23;
+    }
+    else {
+        c = 45;
+    };
+    printf("%d\n",c);
+    return 0;
+}
